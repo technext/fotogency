d99130c2e2d626205d54670abe9dc927d14c3f22
MostofaNobiTanveer <mntanveer9196@gmail.com>
2022-07-04 10:06:48 +0600

    'update'

--- src/js/cursor.js
+++ src/js/cursor.js
@@ -8,16 +8,9 @@ const cursorInit = () => {
     'textarea',
   ]);
 
-  document.addEventListener(
-    'mousemove',
-    (e) => {
-      cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
-      if (navigator.userAgent.indexOf('Safari') !== -1) {
-        cursor.style.transition = 'all 0.15s';
-      }
-    },
-    { passive: true }
-  );
+  document.addEventListener('mousemove', (e) => {
+    cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
+  });
 
   targets.forEach((item) => {
     item.addEventListener('mouseover', () => {
